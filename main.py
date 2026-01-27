bb5c9cd286da54f198ecd08c80da6f798a6ac14d
GaKaplan1 <gabrielkaplan3@gmail.com>
2026-01-27 11:03:28 -0300

    Fix: Final CORS wildcard and 120s timeout

--- main.py
+++ main.py
@@ -13,13 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://infotechlatam.com",
-        "http://infotechlatam.com",
-        "https://www.infotechlatam.com",
-        "http://www.infotechlatam.com"
-    ],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
